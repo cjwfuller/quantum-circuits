ceba7f5e7197e709538fe13f4726cf6d21f028df
Chris Fuller <chris@cjwfuller.com>
2014-02-06 05:00:11 +0000

    solve pylint issues on abstract gate class

--- gate.py
+++ gate.py
@@ -1,19 +1,30 @@
+"""Module includes abstract Gate class"""
+
 from abc import abstractmethod
 
 class Gate(object):
+    """Abstract gate class
+
+    Any gate should implement the methods here whether the gate is quantum or
+    not
+    """
     def __new__(cls, *args, **kwargs):
+        """Prevent gate class from being instantiated"""
         if cls is Gate:
-            raise TypeError("gate class may not be instantiated")
+            raise TypeError("Gate class may not be instantiated")
         return object.__new__(cls, *args, **kwargs)
 
     @abstractmethod
-    def get_symbol():
+    def get_symbol(self):
+        """Implementation should return gate symbol e.g. 'H' for 'Hadamard'"""
         pass
 
     @abstractmethod
-    def get_matrix():
+    def get_matrix(self):
+        """Implementation should return gate matrix"""
         pass
 
     @abstractmethod
-    def get_num_qubits():
+    def get_num_qubits(self):
+        """Implementation should return the number of qubits the gate acts on"""
         pass
