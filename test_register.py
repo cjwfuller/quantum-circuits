8296eecf44ecf63ebe2642ad67514e1cb25e8cc1
Chris Fuller <chris@cjwfuller.com>
2014-07-09 07:31:40 +0100

    fix test register class name

--- test_register.py
+++ test_register.py
@@ -1,7 +1,7 @@
 import unittest
 import register
 
-class Test(unittest.TestCase):
+class TestRegister(unittest.TestCase):
     def test_initialise_size(self):
         r = register.Register(3)
         self.assertEqual(len(r.basis), 8)
