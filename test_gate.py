0e37e679212d4758c3951870e1c9bfee2e92ef5a
Chris Fuller <chris@cjwfuller.com>
2014-07-11 21:38:21 +0100

    fix tabs being used in place of tabs

--- test_gate.py
+++ test_gate.py
@@ -3,12 +3,13 @@ import unittest
 import gate
 
 class TestGate(unittest.TestCase):
-	def test_is_unitary(self):
-		qg = gate.QuantumGate(np.matrix('0 1; 1 0', np.complex_))
+    def test_is_unitary(self):
+        qg = gate.QuantumGate(np.matrix('0 1; 1 0', np.complex_))
 
-	def test_is_not_unitary(self):
-		matrix = np.matrix('1 1; 1 0', np.complex_)
-		self.failUnlessRaises(Exception, gate.QuantumGate, matrix)
+
+    def test_is_not_unitary(self):
+        matrix = np.matrix('1 1; 1 0', np.complex_)
+        self.failUnlessRaises(Exception, gate.QuantumGate, matrix)
 
 if __name__ == '__main__':
     unittest.main()
