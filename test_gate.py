45e9c2d41efb51e10bc7f1cab4b05529d6001c7c
Chris Fuller <chris@cjwfuller.com>
2014-07-11 20:15:38 +0100

    remove call to is_unitary in test gate
    not needed because:
    	- it can't be called from this scope anymore
    	- is_unitary throws an exception if its not unitary

--- test_gate.py
+++ test_gate.py
@@ -5,7 +5,6 @@ import gate
 class TestGate(unittest.TestCase):
 	def test_is_unitary(self):
 		qg = gate.QuantumGate(np.matrix('0 1; 1 0', np.complex_))
-		self.assertTrue(qg.is_unitary())
 
 	def test_is_not_unitary(self):
 		matrix = np.matrix('1 1; 1 0', np.complex_)
