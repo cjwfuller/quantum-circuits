921897428729c8e7cc2e0b1a3e89b1f5ac7da17c
Chris Fuller <chris@cjwfuller.com>
2014-07-13 11:33:08 +0100

    add more is_unitary tests with pauli and cnot gates

--- test_gate.py
+++ test_gate.py
@@ -3,9 +3,20 @@ import unittest
 import gate
 
 class TestGate(unittest.TestCase):
-    def test_is_unitary(self):
+    def test_paulix_is_unitary(self):
         qg = gate.QuantumGate(np.matrix('0 1; 1 0', np.complex_))
 
+    def test_pauliy_is_unitary(self):
+        qg = gate.QuantumGate(np.matrix('0 -1i; 1i 0', np.complex_))
+
+    def test_pauliz_is_unitary(self):
+        qg = gate.QuantumGate(np.matrix('1 0; 0 -1', np.complex_))
+
+    def test_swap_is_unitary(self):
+        qg = gate.QuantumGate(np.matrix('1 0 0 0; 0 0 1 0; 0 1 0 0; 0 0 0 1', np.complex_))
+
+    def test_cnot_is_unitary(self):
+        qg = gate.QuantumGate(np.matrix('1 0 0 0; 0 1 0 0; 0 0 0 1; 0 0 1 0', np.complex_))
 
     def test_is_not_unitary(self):
         matrix = np.matrix('1 1; 1 0', np.complex_)
