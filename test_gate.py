2077a9c3bdd3a32f6dfbf17094d17d1479dd2df3
Chris Fuller <chris@cjwfuller.com>
2014-07-14 08:43:19 +0100

    test toffoli gate is unitary

--- test_gate.py
+++ test_gate.py
@@ -18,6 +18,19 @@ class TestGate(unittest.TestCase):
     def test_cnot_is_unitary(self):
         qg = gate.QuantumGate(np.matrix('1 0 0 0; 0 1 0 0; 0 0 0 1; 0 0 1 0', np.complex_))
 
+    def test_toffoli_is_unitary(self):
+        toffoli = np.matrix(
+            '1 0 0 0 0 0 0 0;'
+            '0 1 0 0 0 0 0 0;'
+            '0 0 1 0 0 0 0 0;'
+            '0 0 0 1 0 0 0 0;'
+            '0 0 0 0 1 0 0 0;'
+            '0 0 0 0 0 1 0 0;'
+            '0 0 0 0 0 0 0 1;'
+            '0 0 0 0 0 0 1 0', np.complex_
+        )
+        qg = gate.QuantumGate(toffoli)
+
     def test_is_not_unitary(self):
         matrix = np.matrix('1 1; 1 0', np.complex_)
         self.failUnlessRaises(Exception, gate.QuantumGate, matrix)
