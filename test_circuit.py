f90d32210985ba19bbb97f8f525d06366792fdad
Chris Fuller <chris@cjwfuller.com>
2014-07-18 20:01:38 +0100

    test circuit constructor does not raise exceptions

--- test_circuit.py
+++ test_circuit.py
@@ -2,8 +2,8 @@ import unittest
 import circuit
 
 class TestCircuit(unittest.TestCase):
-    def test_initialise_size(self):
-        c = circuit.Circuit(3, 10)
+    def test_basic_construction(self):
+        c = circuit.Circuit(1, 5)
 
 if __name__ == '__main__':
     unittest.main()
