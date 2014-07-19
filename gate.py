88a6152571cff8d8eabd08cbc38269ade066a179
Chris Fuller <chris@cjwfuller.com>
2014-07-19 14:38:37 +0100

    refactor gate class
    instead of the gate constructor accepting a matrix, it will
    accept a standard quantum gate name.  It will ensure that
    the gate name is known and that the associated matrix is unitary.
    
    this refactor is so we can add methods to the gate class to get
    matrices from (json, python, ...?) files instead of insisting
    that the constructor is given a matrix

--- gate.py
+++ gate.py
@@ -1,8 +1,32 @@
+import math
 import numpy as np
 
 class QuantumGate:
-    def __init__(self, matrix):
-        self.matrix = matrix
+    def __init__(self, name):
+        if name == 'paulix':
+            self.matrix = self.__get_paulix()
+            self.symbol = 'X'
+        elif name == 'pauliy':
+            self.matrix = self.__get_pauliy()
+            self.symbol = 'Y'
+        elif name == 'pauliz':
+            self.matrix = self.__get_pauliz()
+            self.symbol = 'Z'
+        elif name == 'swap':
+            self.matrix = self.__get_swap()
+            self.symbol = 'S'
+        elif name == 'cnot':
+            self.matrix = self.__get_cnot()
+            self.symbol = 'C'
+        elif name == 'toffoli':
+            self.matrix = self.__get_toffoli()
+            self.symbol = 'T'
+        elif name == 'hadamard':
+            self.matrix = self.__get_hadamard()
+            self.symbol = 'H'
+        else:
+            raise Exception("Supplied gate name is unknown")
+
         if(not self.__is_unitary()):
             raise Exception("Supplied matrix is not unitary")
 
@@ -16,3 +40,33 @@ class QuantumGate:
         identity = np.identity(x)
         conjugate_transpose = self.matrix.getH()
         return (self.matrix * conjugate_transpose).all() == identity.all()
+
+    def __get_paulix(self):
+        return np.matrix('0 1; 1 0', np.complex_)
+
+    def __get_pauliy(self):
+        return np.matrix('0 -1i; 1i 0', np.complex_)
+
+    def __get_pauliz(self):
+        return np.matrix('1 0; 0 -1', np.complex_)
+
+    def __get_swap(self):
+        return np.matrix('1 0 0 0; 0 0 1 0; 0 1 0 0; 0 0 0 1', np.complex_)
+
+    def __get_hadamard(self):
+        return ((1/math.sqrt(2)+0j) * np.matrix('1 1; 1 -1', np.complex_))
+
+    def __get_cnot(self):
+        return np.matrix('1 0 0 0; 0 1 0 0; 0 0 0 1; 0 0 1 0', np.complex_)
+
+    def __get_toffoli(self):
+        return np.matrix(
+            '1 0 0 0 0 0 0 0;'
+            '0 1 0 0 0 0 0 0;'
+            '0 0 1 0 0 0 0 0;'
+            '0 0 0 1 0 0 0 0;'
+            '0 0 0 0 1 0 0 0;'
+            '0 0 0 0 0 1 0 0;'
+            '0 0 0 0 0 0 0 1;'
+            '0 0 0 0 0 0 1 0', np.complex_
+        )
