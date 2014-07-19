6f4db3a59e1f5c918cb04a13fba7270f7edea583
Chris Fuller <chris@cjwfuller.com>
2014-07-19 14:37:15 +0100

    test refactor of gate class

--- test_gate.py
+++ test_gate.py
@@ -3,44 +3,14 @@ import unittest
 import gate
 
 class TestGate(unittest.TestCase):
-    def test_paulix_is_unitary(self):
-        qg = gate.QuantumGate(np.matrix('0 1; 1 0', np.complex_))
-
-    def test_pauliy_is_unitary(self):
-        qg = gate.QuantumGate(np.matrix('0 -1i; 1i 0', np.complex_))
-
-    def test_pauliz_is_unitary(self):
-        qg = gate.QuantumGate(np.matrix('1 0; 0 -1', np.complex_))
-
-    def test_swap_is_unitary(self):
-        qg = gate.QuantumGate(np.matrix('1 0 0 0; 0 0 1 0; 0 1 0 0; 0 0 0 1', np.complex_))
-
-    def test_cnot_is_unitary(self):
-        qg = gate.QuantumGate(np.matrix('1 0 0 0; 0 1 0 0; 0 0 0 1; 0 0 1 0', np.complex_))
-
-    def test_toffoli_is_unitary(self):
-        toffoli = np.matrix(
-            '1 0 0 0 0 0 0 0;'
-            '0 1 0 0 0 0 0 0;'
-            '0 0 1 0 0 0 0 0;'
-            '0 0 0 1 0 0 0 0;'
-            '0 0 0 0 1 0 0 0;'
-            '0 0 0 0 0 1 0 0;'
-            '0 0 0 0 0 0 0 1;'
-            '0 0 0 0 0 0 1 0', np.complex_
-        )
-        qg = gate.QuantumGate(toffoli)
-
-    def test_hadamard_is_unitary(self):
-        qg = gate.QuantumGate(((1/math.sqrt(2)+0j) * np.matrix('1 1; 1 -1', np.complex_)))
-
-    def test_all_real_is_not_unitary(self):
-        matrix = np.matrix('1 1; 1 0', np.complex_)
-        self.failUnlessRaises(Exception, gate.QuantumGate, matrix)
-
-    def test_complex_is_not_unitary(self):
-        matrix = np.matrix('1 -1i; 1 -1i', np.complex_)
-        self.failUnlessRaises(Exception, gate.QuantumGate, matrix)
+    def test_standard_gates_implemented(self):
+        gate.QuantumGate('paulix')
+        gate.QuantumGate('pauliy')
+        gate.QuantumGate('pauliz')
+        gate.QuantumGate('swap')
+        gate.QuantumGate('cnot')
+        gate.QuantumGate('toffoli')
+        gate.QuantumGate('hadamard')
 
 if __name__ == '__main__':
     unittest.main()
