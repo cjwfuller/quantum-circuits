19dd80dc3f583f3ce4fec2f439929628d9bb1156
Chris Fuller <chris@cjwfuller.com>
2014-07-20 10:43:17 +0100

    remove test circuit
    the circuit has been refactored into "conceptual circuit" and
    low-level "quantum circuit".  The tests need to be refactored
    into separate files

--- test_circuit.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import circuit
-
-class TestCircuit(unittest.TestCase):
-    def test_basic_construction(self):
-        c = circuit.Circuit(1, 5)
-
-if __name__ == '__main__':
-    unittest.main()
