d75dc845a456782549d00364e2637c7c32b8507a
Chris Fuller <chris@cjwfuller.com>
2014-07-22 07:22:12 +0100

    add additional gate tests

--- test_gate.py
+++ test_gate.py
@@ -12,5 +12,30 @@ class TestGate(unittest.TestCase):
         gate.QuantumGate('toffoli')
         gate.QuantumGate('hadamard')
 
+    def test_implemented_gates_only_constraint(self):
+        self.assertRaises(Exception, gate.QuantumGate, 'foo')
+
+    def test_gates_have_alphabetic_symbols(self):
+        paulix = gate.QuantumGate('paulix')
+        self.assertEquals('X', paulix.symbol)
+
+        pauliy = gate.QuantumGate('pauliy')
+        self.assertEquals('Y', pauliy.symbol)
+
+        pauliz = gate.QuantumGate('pauliz')
+        self.assertEquals('Z', pauliz.symbol)
+
+        swap = gate.QuantumGate('swap')
+        self.assertEquals('S', swap.symbol)
+
+        cnot = gate.QuantumGate('cnot')
+        self.assertEquals('C', cnot.symbol)
+
+        toffoli = gate.QuantumGate('toffoli')
+        self.assertEquals('T', toffoli.symbol)
+
+        hadamard = gate.QuantumGate('hadamard')
+        self.assertEquals('H', hadamard.symbol)
+
 if __name__ == '__main__':
     unittest.main()
