f014bef2cc767ce7e48bdbfaeda8c71e5b3c45c3
Chris Fuller <chris@cjwfuller.com>
2014-07-24 07:30:46 +0100

    add basic quantum gate tests with todos

---
+++ test_quantum_circuit.py
@@ -0,0 +1,42 @@
+import unittest
+import circuit
+import gate
+
+class TestCircuit(unittest.TestCase):
+    def test_basic_construction(self):
+        c = circuit.Circuit(1, 5)
+
+    def test_circuit_construction_size(self):
+        c = circuit.Circuit(1, 5)
+
+        num_basis = 2
+        num_steps = 5
+
+        self.assertEqual(len(c.grid), num_basis)
+        for x in c.grid:
+            self.assertEqual(len(x), num_steps)
+
+    def test_add_gate(self):
+        c = circuit.Circuit(1, 5)
+        basis = 0
+        hadamard = gate.QuantumGate('hadamard')
+        c.add_gate(hadamard, 1)
+
+    # TODO
+    def test_add_after_last_step_constraint(self):
+        self.fail()
+
+    # TODO
+    def test_add_gate_posn_constraint(self):
+        self.fail()
+
+    # TODO
+    def test_add_gate_too_large_constraint(self):
+        self.fail()
+
+    # TODO
+    def test_add_gate_too_small_constraint(self):
+        self.fail()
+
+if __name__ == '__main__':
+    unittest.main()
