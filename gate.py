364b6aeb05ec5b99408276b9be490d1ef81ff5dc
Chris Fuller <chris@cjwfuller.com>
2014-07-25 21:38:06 +0100

    refactor gate constructor
    moved gate matrices into constructor as they're only 1-liners
    and the symbols were getting assigned in the constructor

--- gate.py
+++ gate.py
@@ -4,26 +4,35 @@ import numpy as np
 class QuantumGate:
     def __init__(self, name):
         if name == 'paulix':
-            self.matrix = self.__get_paulix()
             self.symbol = 'X'
+            self.matrix = np.matrix('0 1; 1 0', np.complex_)
         elif name == 'pauliy':
-            self.matrix = self.__get_pauliy()
             self.symbol = 'Y'
+            self.matrix = np.matrix('0 -1i; 1i 0', np.complex_)
         elif name == 'pauliz':
-            self.matrix = self.__get_pauliz()
             self.symbol = 'Z'
+            self.matrix = np.matrix('1 0; 0 -1', np.complex_)
         elif name == 'swap':
-            self.matrix = self.__get_swap()
             self.symbol = 'S'
+            self.matrix = np.matrix('1 0 0 0; 0 0 1 0; 0 1 0 0; 0 0 0 1', np.complex_)
         elif name == 'cnot':
-            self.matrix = self.__get_cnot()
             self.symbol = 'C'
-        elif name == 'toffoli':
-            self.matrix = self.__get_toffoli()
-            self.symbol = 'T'
+            self.matrix = np.matrix('1 0 0 0; 0 1 0 0; 0 0 0 1; 0 0 1 0', np.complex_)
         elif name == 'hadamard':
-            self.matrix = self.__get_hadamard()
             self.symbol = 'H'
+            self.matrix = ((1/math.sqrt(2)+0j) * np.matrix('1 1; 1 -1', np.complex_))
+        elif name == 'toffoli':
+            self.symbol = 'T'
+            self.matrix = np.matrix(
+            '1 0 0 0 0 0 0 0;'
+            '0 1 0 0 0 0 0 0;'
+            '0 0 1 0 0 0 0 0;'
+            '0 0 0 1 0 0 0 0;'
+            '0 0 0 0 1 0 0 0;'
+            '0 0 0 0 0 1 0 0;'
+            '0 0 0 0 0 0 0 1;'
+            '0 0 0 0 0 0 1 0', np.complex_
+        )
         else:
             raise Exception("Supplied gate name is unknown")
 
@@ -40,33 +49,3 @@ class QuantumGate:
         identity = np.identity(x)
         conjugate_transpose = self.matrix.getH()
         return (self.matrix * conjugate_transpose).all() == identity.all()
-
-    def __get_paulix(self):
-        return np.matrix('0 1; 1 0', np.complex_)
-
-    def __get_pauliy(self):
-        return np.matrix('0 -1i; 1i 0', np.complex_)
-
-    def __get_pauliz(self):
-        return np.matrix('1 0; 0 -1', np.complex_)
-
-    def __get_swap(self):
-        return np.matrix('1 0 0 0; 0 0 1 0; 0 1 0 0; 0 0 0 1', np.complex_)
-
-    def __get_hadamard(self):
-        return ((1/math.sqrt(2)+0j) * np.matrix('1 1; 1 -1', np.complex_))
-
-    def __get_cnot(self):
-        return np.matrix('1 0 0 0; 0 1 0 0; 0 0 0 1; 0 0 1 0', np.complex_)
-
-    def __get_toffoli(self):
-        return np.matrix(
-            '1 0 0 0 0 0 0 0;'
-            '0 1 0 0 0 0 0 0;'
-            '0 0 1 0 0 0 0 0;'
-            '0 0 0 1 0 0 0 0;'
-            '0 0 0 0 1 0 0 0;'
-            '0 0 0 0 0 1 0 0;'
-            '0 0 0 0 0 0 0 1;'
-            '0 0 0 0 0 0 1 0', np.complex_
-        )
