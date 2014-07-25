4d709caeca6db008e08288285d99a0b2f9a2b4ef
Chris Fuller <chris@cjwfuller.com>
2014-07-25 21:24:42 +0100

    rename incorrect register variables

--- register.py
+++ register.py
@@ -1,3 +1,3 @@
 class Register:
     def __init__(self, num_qubits):
-        self.basis = [complex(0,0)] * pow(2, num_qubits)
+        self.state = [complex(0,0)] * pow(2, num_qubits)
--- test_register.py
+++ test_register.py
@@ -4,7 +4,7 @@ import register
 class TestRegister(unittest.TestCase):
     def test_initialise_size(self):
         r = register.Register(3)
-        self.assertEqual(len(r.basis), 8)
+        self.assertEqual(len(r.state), 8)
 
 if __name__ == '__main__':
     unittest.main()
