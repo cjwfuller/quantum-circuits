bc96f810552cc6fd9c0495d1200d5f10204fd819
Chris Fuller <chris@cjwfuller.com>
2014-08-23 12:36:38 +0100

    implement skeleton quantum circuit tests

--- test_quantum_circuit.py
+++ test_quantum_circuit.py
@@ -1,52 +1,116 @@
 import unittest
-import quantum_circuit
+import quantum_circuit as qc
 import gate
 
 class TestQuantumCircuit(unittest.TestCase):
+
     def test_basic_construction(self):
-        c = circuit.Circuit(1, 5)
+        """Construction should not raise any exceptions"""
+        c = qc.QuantumCircuit(1, 5)
 
-    def test_circuit_construction_size(self):
-        c = circuit.Circuit(1, 5)
+    def test_qc_construction_size(self):
+        """"Construction should make a quantum circuit correctly
 
+        The number of steps, number of qubits and gate dimensions should all
+        be correct"""
+        num_qubits = 1
         num_basis = 2
         num_steps = 5
 
-        self.assertEqual(len(c.grid), num_basis)
-        for x in c.grid:
-            self.assertEqual(len(x), num_steps)
+        c = qc.QuantumCircuit(num_qubits, num_steps)
+
+        self.assertEqual(len(c.grid), num_steps)
+        for step in c.grid:
+            for gate in step:
+                self.assertEqual(len(gate), num_basis)
 
     def test_add_gate(self):
-        c = circuit.Circuit(1, 5)
-        basis = 0
-        hadamard = gate.QuantumGate('hadamard')
-        c.add_gate(hadamard, 1)
+        """Adding a quantum gate, works
 
-    def test_step_forwards(self):
-        c = quantum_circuit.QuantumCircuit(1, 5)
+        When a quantum gate is added, the correct gate should be added to the
+        correct place"""
+        num_qubits = 1
+        num_steps = 5
+        qubit_num = 0
+        step = 0
+
+        c = qc.QuantumCircuit(num_qubits, num_steps)
+        paulix = gate.QuantumGate('paulix')
+        c.add_gate(paulix, qubit_num)
+
+        self.assertEqual(c.grid[step][qubit_num].symbol, paulix.symbol)
+
+    def test_two_gates_same_qubit(self):
+        """Adding two quantum gates to the same first qubit, works
+
+        The second gate should be added after the first on the same qubit
+        """
+        num_qubits = 1
+        num_steps = 5
+        qubit_num = 0
+        paulix_step = 0
+        pauliy_step = 1
+
+        c = qc.QuantumCircuit(num_qubits, num_steps)
+        paulix = gate.QuantumGate('paulix')
+        pauliy = gate.QuantumGate('pauliy')
+        c.add_gate(paulix, qubit_num)
+        c.add_gate(pauliy, qubit_num)
+
+        self.assertEqual(c.grid[paulix_step][qubit_num].symbol, paulix.symbol)
+        self.assertEqual(c.grid[pauliy_step][qubit_num].symbol, pauliy.symbol)
+
+    def test_add_after_last_step_constraint(self):
+        """Adding a gate after last position, fails"""
+        num_qubits = 1
+        num_steps = 2
+        qubit_num = 0
+        paulix_step = 0
+        pauliy_step = 1
+
+        c = qc.QuantumCircuit(num_qubits, num_steps)
+        paulix = gate.QuantumGate('paulix')
+        pauliy = gate.QuantumGate('pauliy')
+        c.add_gate(paulix, qubit_num)
+
+        self.assertRaises(Exception, c.add_gate, paulix, 1)
+
+    def test_add_gate_posn_constraint(self):
+        """Adding a gate to invalid position, fails"""
+        num_qubits = 1
+        num_steps = 2
+
+        c = qc.QuantumCircuit(num_qubits, num_steps)
+        paulix = gate.QuantumGate('paulix')
+
+        self.assertRaises(Exception, c.add_gate, paulix, 2)
+
+    def test_add_gate_too_large_constraint(self):
+        """Adding a gate too big for circuit, fails
+
+        Adding a Hadamard gate (2-qubits) to a 1-qubit system, fails
+        """
+        num_qubits = 1
+        num_steps = 2
+
+        c = qc.QuantumCircuit(num_qubits, num_steps)
+        hadamard = gate.QuantumGate('hadamard')
+
+        self.assertRaises(Exception, c.add_gate, hadamard, 1)
+
+    def test_maintain_step(self):
+        """Stepping forwards, maintains the circuit position
+
+        When the circuit is moved forwards, the step should be incremented
+        """
+        num_qubits = 1
+        num_steps = 5
+
+        c = qc.QuantumCircuit(num_qubits, num_steps)
+
         self.assertEqual(c.step, 0)
         c.step_forwards()
         self.assertEqual(c.step, 1)
-
-    def test_display(self):
-        c = quantum_circuit.QuantumCircuit(1, 2)
-        c.display()
-
-    # TODO
-    def test_add_after_last_step_constraint(self):
-        self.fail()
-
-    # TODO
-    def test_add_gate_posn_constraint(self):
-        self.fail()
-
-    # TODO
-    def test_add_gate_too_large_constraint(self):
-        self.fail()
-
-    # TODO
-    def test_add_gate_too_small_constraint(self):
-        self.fail()
 
 if __name__ == '__main__':
     unittest.main()
