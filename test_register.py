df29dd93c96f1c4159f8f75379a40ca8c03eb0f0
Chris Fuller <chris@cjwfuller.com>
2014-08-30 17:38:50 +0100

    implement some register tests

--- test_register.py
+++ test_register.py
@@ -10,7 +10,6 @@ class TestRegister(unittest.TestCase):
         state = np.array([1, 0], dtype=np.complex_)
         r = register.Register(state)
 
-    # TODO
     def test_superposition_construction(self):
         """Constructing a valid register with superposition, works"""
         state = np.array([1/math.sqrt(2), 1/math.sqrt(2)], dtype=np.complex_)
@@ -20,9 +19,10 @@ class TestRegister(unittest.TestCase):
     def test_invalid_size_constraint(self):
         """Constructing register with invalid size, fails"""
 
-    # TODO
     def test_invalid_state_constraint(self):
         """Constructing register with invalid state, fails"""
+        state = np.array([1, 1, 1, 1], dtype=np.complex_)
+        self.assertRaises(Exception, register.Register, state)
 
 if __name__ == '__main__':
     unittest.main()
