61b757a1fec5155a959f7fa16531c9703f927702
Chris Fuller <chris@cjwfuller.com>
2014-12-12 19:49:49 +0000

    split up gate tests

--- tests/test_gate.py
+++ tests/test_gate.py
@@ -15,25 +15,31 @@ class TestGate(unittest.TestCase):
     def test_implemented_gates_only_constraint(self):
         self.assertRaises(Exception, gate.QuantumGate, 'foo')
 
-    def test_gates_have_alphabetic_symbols(self):
+    def test_paulix_symbol(self):
         paulix = gate.QuantumGate('paulix')
         self.assertEquals('X', paulix.symbol)
 
+    def test_pauliy_symbol(self):
         pauliy = gate.QuantumGate('pauliy')
         self.assertEquals('Y', pauliy.symbol)
 
+    def test_pauliz_symbol(self):
         pauliz = gate.QuantumGate('pauliz')
         self.assertEquals('Z', pauliz.symbol)
 
+    def test_swap_symbol(self):
         swap = gate.QuantumGate('swap')
         self.assertEquals('S', swap.symbol)
 
+    def test_cnot_symbol(self):
         cnot = gate.QuantumGate('cnot')
         self.assertEquals('C', cnot.symbol)
 
+    def test_toffoli_symbol(self):
         toffoli = gate.QuantumGate('toffoli')
         self.assertEquals('T', toffoli.symbol)
 
+    def test_hadamard_symbol(self):
         hadamard = gate.QuantumGate('hadamard')
         self.assertEquals('H', hadamard.symbol)
 
