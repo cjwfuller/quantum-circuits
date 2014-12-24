9bf7716ac23140c4177a9efd3171ba908e943e70
Chris Fuller <chris@cjwfuller.com>
2014-12-24 11:43:20 +0000

    implement gate resize tests

--- tests/test_gate.py
+++ tests/test_gate.py
@@ -1,9 +1,25 @@
 import unittest
-import paulix
+import numpy as np
+import cnot
 
 class TestGate(unittest.TestCase):
     def test_basic_resize(self):
         """Re-sizing a gate by making it act on more qubits, works"""
+        gate = cnot.CNOTQuantumGate()
+        gate.resize(3, [0, 2])
+        shape = np.shape(gate.matrix)
+        size = shape[0]
+        self.assertEquals(8, size)
+        self.assertEquals(np.matrix("""
+               1 0 0 0 0 0 0 0;
+               0 1 0 0 0 0 0 0;
+               0 0 1 0 0 0 0 0;
+               0 0 0 1 0 0 0 0;
+               0 0 0 0 0 1 0 0;
+               0 0 0 0 1 0 0 0;
+               0 0 0 0 0 0 0 1;
+               0 0 0 0 0 0 1 0
+            """, np.complex_).all(), gate.matrix.all())
 
     def test_non_integer_components_gate_reize(self):
         """Re-sizing a gate with non-integer real/imaginary components,
@@ -11,14 +27,23 @@ class TestGate(unittest.TestCase):
 
     def test_shrink_constraint(self):
         """Making an n-qubit gate act on m whith m < n, fails"""
-
-    def test_resize_too_large_constraint(self):
-        """Re-sizing a gate to n qubits for a qubit system of size m where n >
-        m, fails"""
+        gate = cnot.CNOTQuantumGate()
+        self.assertRaises(Exception, gate.resize, 1, [0, 2])
 
     def test_non_resize(self):
         """Re-sizing an n-qubit gate to an n-qubit gate does nothing to the
         gate"""
+        gate = cnot.CNOTQuantumGate()
+        gate.resize(2, [0, 2])
+        shape = np.shape(gate.matrix)
+        size = shape[0]
+        self.assertEquals(4, size)
+        self.assertEquals(np.matrix("""
+            1 0 0 0;
+            0 1 0 0;
+            0 0 0 1;
+            0 0 1 0
+        """, np.complex_).all(), gate.matrix.all())
 
 if __name__ == '__main__':
     unittest.main()
