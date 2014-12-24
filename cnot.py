be0211fcc594799013c12d652df3897734740ea3
Chris Fuller <chris@cjwfuller.com>
2014-12-24 12:35:29 +0000

    add CNOT gate

---
+++ cnot.py
@@ -0,0 +1,11 @@
+import numpy as np
+import quantum_gate
+
+class CNOTQuantumGate(quantum_gate.QuantumGate):
+    def __init__(self):
+        self.symbol = 'C'
+        self.matrix = np.matrix("""
+            1 0 0 0;
+            0 1 0 0;
+            0 0 0 1;
+            0 0 1 0""", np.complex_)
