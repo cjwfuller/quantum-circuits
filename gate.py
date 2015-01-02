78ec3fa80d90b22b9576de0c11919bec1aa7673e
Chris Fuller <chris@cjwfuller.com>
2015-01-02 13:08:19 +0000

    ensure gate class cannot be instantiated

--- gate.py
+++ gate.py
@@ -1,6 +1,11 @@
 from abc import abstractmethod
 
 class Gate(object):
+    def __new__(cls, *args, **kwargs):
+        if cls is Gate:
+            raise TypeError("gate class may not be instantiated")
+        return object.__new__(cls, *args, **kwargs)
+
     @abstractmethod
     def get_symbol():
         pass
