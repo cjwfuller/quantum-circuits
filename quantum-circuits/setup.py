39629d3d6a1c8ba0e64083aa85091ba88a1ada5e
Chris Fuller <chris@cjwfuller.com>
2015-07-01 17:58:04 +0100

    move code into module

---
+++ quantum-circuits/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+        name = "Quantum Circuits",
+        version = "1.0.0",
+        description = "Quantum Circuits",
+        author = "Chris Fuller",
+        author_email = "chris@cjwfuller.com",
+        url = "https://github.com/cjwfuller/quantum-circuits",
+        install_requires = ["numpy >= 1.9.1", "unittest2 >= 1.0.1"]
+)
